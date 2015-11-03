d6d63b601c5d98b6e0c667531b3b81a73ad95e63
Zascia <zascia@ukr.net>
2015-11-03 16:37:19 +0200

    adding show - hide details feature

--- epam/js/modules/carousel.js
+++ epam/js/modules/carousel.js
@@ -9,6 +9,13 @@ define(['underscore', 'jquery'], function(_, $) {
         var currentIndex = 0;
         var currentActive =  $(obj).hide().get(currentIndex);
 
+        function showDetails(obj) {
+            var currentActive = obj;
+            $(currentActive).find('.info-box-image').toggle('50');
+            $(currentActive).find('.info-box-details').toggle('50');
+            $(currentActive).find('.more-link').toggle('50');
+        }
+
         $(currentActive).show();
         $(currentActive).one('click', '.nav-link', function singleClick(e) {
             e.preventDefault();
@@ -20,8 +27,16 @@ define(['underscore', 'jquery'], function(_, $) {
                 currentActive = nextActive;
             }
             $(currentActive).one('click', '.nav-link', singleClick);
+            $(currentActive).on('click', '.more-link', function(e) {
+                e.preventDefault();
+                showDetails($(currentActive));
+            });
         });
 
+        $(currentActive).on('click', '.more-link', function(e) {
+            e.preventDefault();
+            showDetails($(currentActive));
+        });
 
     };
 
