8730c60559f4ffeb7e48f8a345712cac484c1a70
Zascia <zascia@ukr.net>
2015-11-06 11:09:42 +0200

    Addging feature to close current details before jump to next

--- epam/js/modules/carousel.js
+++ epam/js/modules/carousel.js
@@ -9,18 +9,29 @@ define(['underscore', 'jquery'], function(_, $) {
         var currentIndex = 0;
         var currentActive =  $(obj).hide().get(currentIndex);
 
-        function showDetails(obj) {
-            var currentActive = obj;
+        function showDetails() {
+            var hideLinkPos = $(currentActive).find('.more-link-container').scrollTop();
+            console.log('hideLinkPos', hideLinkPos);
             $(currentActive).find('.info-box-content').toggleClass('activated');
             $(currentActive).find('.info-box-image').toggle();
             $(currentActive).find('.info-box-details').toggle();
-            $(currentActive).find('.more-link').toggle();
+            $(currentActive).find('.more-link').toggle("", function() {
+                /*console.log('enter into move link back func', $(this).parent().scrollTop() - hideLinkPos);
+                if ( $(this).hasClass('hide-details') && $(this).is(':visible') ) {
+                    $(this).parent().scrollTo(200);
+                }*/
+            });
         }
 
         $(currentActive).show();
         $(currentActive).one('click', '.nav-link', function singleClick(e) {
             e.preventDefault();
             if (!$(this).hasClass('nav-last')) {
+
+                if ( $(currentActive).find('.hide-details').is(':visible') ) {
+                    showDetails($(currentActive));
+                }
+
                 direction = $(this).hasClass('nav-prev') ? currentIndex-- : currentIndex++;
                 var nextActive = $(obj).get(currentIndex);
                 $(currentActive).toggle();
@@ -28,15 +39,13 @@ define(['underscore', 'jquery'], function(_, $) {
                 currentActive = nextActive;
             }
             $(currentActive).one('click', '.nav-link', singleClick);
-            $(currentActive).on('click', '.more-link', function(e) {
-                e.preventDefault();
-                if ( $(this).is(':visible') ) showDetails($(currentActive));
-            });
+
         });
 
-        $(currentActive).on('click', '.more-link', function(e) {
+        $(obj).on('click', '.more-link', function(e) {
+            console.log('obj click event and this = ', $(this));
             e.preventDefault();
-            if ( $(this).is(':visible') ) showDetails($(currentActive));
+            if ( $(this).is(':visible') ) showDetails();
         });
 
     };
